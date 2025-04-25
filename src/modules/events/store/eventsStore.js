3b8699f02556889c91573773253887c465af78c7
Kmcpoma <70334639+Kmcpoma@users.noreply.github.com>
2025-04-25 00:08:33 -0400

    cambios de event

--- src/modules/events/store/eventsStore.js
+++ src/modules/events/store/eventsStore.js
@@ -22,7 +22,6 @@ export const useEventsStore = defineStore('events', {
       this.events.push(newEvent)
       this.applyFilters()
     },
-
     // Actualizar un evento existente
     async updateEvent(id, updatedEvent) {
       const updated = await updateEvent(id, updatedEvent)
